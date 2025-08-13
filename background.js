85635cf08fb380a6032cdd869674c96c0906799d
Daltoniks <157841614+Fae1337@users.noreply.github.com>
2025-08-13 15:23:25 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,15 +1,19 @@
 chrome.webNavigation.onCommitted.addListener(function (details) {
+    // Запрещает запуск скрипта во время загрузки других фреймов
     if (details.frameId == 0) {
 
         chrome.tabs.get(details.tabId, function(tab) {
             if (!tab || !tab.url) return;
 
+            // Получает URL страницы
             let url = tab.url;
 
+            // Удаляем протокол и www
             let parsedUrl = url.replace("https://", "")
                 .replace("http://", "")
                 .replace("www.", "");
 
+            // Получаем только домен
             let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
                 .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
 
@@ -30,5 +34,4 @@ function runLinkedinScript(tabId) {
         target: { tabId: tabId },
         files: ['ygadblocker.js']
     });
-
 }
