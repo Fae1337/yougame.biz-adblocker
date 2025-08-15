9c8c44922b9268a885d486c658785c59942d357f
Daltoniks <157841614+Fae1337@users.noreply.github.com>
2025-08-15 15:24:46 +0300

    Add files via upload

---
+++ firefox/background.js
@@ -0,0 +1,31 @@
+chrome.webNavigation.onCommitted.addListener(function (details) {
+    chrome.tabs.get(details.tabId, function(tab) {
+        if (!tab || !tab.url) return;
+
+            let url = tab.url;
+
+            let parsedUrl = url.replace("https://", "")
+                .replace("http://", "")
+                .replace("www.", "");
+
+            let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
+                .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
+
+            try {
+                if (domain && domain === "yougame.biz") {
+                    runLinkedinScript(details.tabId);
+                }
+            } catch (err) {
+                console.error(err);
+            }
+
+        });
+    }
+);
+
+function runLinkedinScript(tabId) {
+    chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ['ygadblocker.js']
+    });
+}
